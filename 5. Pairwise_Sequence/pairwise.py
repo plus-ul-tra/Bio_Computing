a1f3cf54e17c837b44e0b9aa9af68bff337714a4
홍한울 <honghan3235@gmail.com>
2024-04-17 21:55:30 +0900

    5,6

---
+++ 5. Pairwise_Sequence/pairwise.py
@@ -0,0 +1,41 @@
+import sys
+import time
+
+def get_input_data(filename):
+    
+
+
+    return 0
+
+def pairwise_sequence():
+
+    return 0
+
+def output_data(filename, data):
+
+    return 0
+
+
+def main():
+    input_filename ='input.txt'
+    output_filename ='output.txt'
+    #input_filename = sys.argv[1]    
+    #output_filename = sys.argv[2]
+
+    dna_sequence = get_input_data(input_filename)
+
+    #start_time= time.time()
+    pairwise_sequence()
+    #end_time = time.time()
+
+    #elapsed_time = end_time - start_time
+    
+    output_data(output_filename, edit_distance)
+    
+    #print(f"Elapsed Time: {elapsed_time:f} microseconds")
+
+
+    return 0
+
+if __name__ == '__main__':
+    main()
